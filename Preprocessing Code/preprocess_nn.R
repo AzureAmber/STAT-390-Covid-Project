48f7c8de6d3e5b684c4be34e954548b035d3d9e0
AzureAmber <TokyoGhoul4life1@gmail.com>
2023-10-22 01:04:30 -0500

    added clustering

--- Preprocessing Code/preprocess_nn.R
+++ Preprocessing Code/preprocess_nn.R
@@ -23,8 +23,8 @@ data_cur = data %>%
 results = data_cur %>% skim_without_charts()
 names_filt = results$skim_variable[results$complete_rate < 0.7]
 names_filtn = setdiff(names_filt,
-                      c('icu_patients', 'hosp_patients', 'total_tests', 'new_tests',
-                        'positive_rate', 'total_vaccinations', 'people_vaccinated'))
+                      c('total_tests', 'new_tests', 'positive_rate',
+                        'total_vaccinations', 'people_vaccinated'))
 names_col = c('iso_code', 'median_age', 'aged_65_older', 'aged_70_older',
               'human_development_index', 'new_cases_smoothed', 'new_deaths_smoothed',
               'new_deaths_smoothed_per_million', 'new_cases_smoothed_per_million')
@@ -33,31 +33,25 @@ data_nn = data_cur %>% select(-any_of(c(names_filtn, names_col)))
 
 
 
-# Imputation for large column missingness: Neural Network = create booleann indicators
+# Imputation for large column missingness: Neural Network = create boolean indicators
+# TRUE if the entry has value and False if the entry is missing
 data_nn2 = data_nn %>%
   mutate(
-    icu_patients_b = ifelse(is.na(icu_patients) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                            FALSE, TRUE),
-    hosp_patients_b = ifelse(is.na(hosp_patients) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                             FALSE, TRUE),
-    total_tests_b = ifelse(is.na(total_tests) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                           FALSE, TRUE),
-    new_tests_b = ifelse(is.na(new_tests) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                         FALSE, TRUE),
-    positive_rate_b = ifelse(is.na(positive_rate) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                             FALSE, TRUE),
-    total_vaccinations_b = ifelse(is.na(total_vaccinations) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                                  FALSE, TRUE),
-    people_vaccinated_b = ifelse(is.na(people_vaccinated) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                                 FALSE, TRUE)
+    total_tests_b = ifelse(is.na(total_tests), FALSE, TRUE),
+    new_tests_b = ifelse(is.na(new_tests), FALSE, TRUE),
+    positive_rate_b = ifelse(is.na(positive_rate), FALSE, TRUE),
+    total_vaccinations_b = ifelse(is.na(total_vaccinations), FALSE, TRUE),
+    people_vaccinated_b = ifelse(is.na(people_vaccinated), FALSE, TRUE)
   )
 # SPLIT INTO TRAINING AND TESTING SET HERE FOR THE ABOVE DATAS
 
 train_nn  <- data_nn2 |> arrange(date) %>% filter(date < as.Date("2023-01-01"))
 test_nn <- data_nn2 |> arrange(date) %>% filter(date >= as.Date("2023-01-01"))
 
-write_rds(train_nn, 'data/processed_data/train_nn.rds')
-write_rds(test_nn, 'data/processed_data/test_nn.rds')
+# write_rds(train_nn, 'data/processed_data/train_nn.rds')
+# write_rds(test_nn, 'data/processed_data/test_nn.rds')
+
+
 
 # Imputation for random missingness
 # Either impute using process below   OR    impute by clustering
@@ -115,19 +109,17 @@ data_nn3 = data_nn3 %>%
 
 data_nn3 = data_nn3 %>%
   group_by(location) %>%
-  fill(icu_patients, .direction = "downup") %>%
-  fill(hosp_patients, .direction = "downup") %>%
   fill(total_tests, .direction = "downup") %>%
   fill(positive_rate, .direction = "downup") %>%
   fill(total_vaccinations, .direction = "downup") %>%
   fill(people_vaccinated, .direction = "downup") %>%
   mutate(new_tests = ifelse(is.na(new_tests), 0, new_tests)) %>%
   ungroup()
-# NOTE: hosp_patients and icu_patients have missingness only due to countries not reporting
 
 
 # View(data_nn3 %>% skim_without_charts())
 # View(cor(data_nn3 %>% select(-c(continent, location, date, G20, G24))))
+
 
 
 #   ***** Impute with Clustering *****
