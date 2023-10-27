2db47c6d04a9e6b86f8db1f72cacd3c271d95227
AzureAmber <TokyoGhoul4life1@gmail.com>
2023-10-26 20:41:00 -0500

    Midterm Report: added acf analysis

--- Preprocessing Code/preprocess_tree.R
+++ Preprocessing Code/preprocess_tree.R
@@ -45,9 +45,7 @@ data_tree2 = data_tree %>%
     positive_rate = ifelse(is.na(positive_rate) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
                            v, positive_rate),
     total_vaccinations = ifelse(is.na(total_vaccinations) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                                v, total_vaccinations),
-    people_vaccinated = ifelse(is.na(people_vaccinated) & (date < as.Date("2021-02-01") | date > as.Date("2022-03-01")),
-                               v, people_vaccinated)
+                                v, total_vaccinations)
   )
 # SPLIT INTO TRAINING AND TESTING SET HERE FOR THE ABOVE DATAS
 
@@ -63,75 +61,8 @@ test_tree <- data_tree2 |> arrange(date) %>% filter(date >= as.Date("2023-01-01"
 
 
 
-# Imputation for random missingness
-# Either impute using process below   OR    impute by clustering
-# - new_deaths impute with 0, total deaths by last non-zero value
-# - total_cases by last non-zero value, new_cases by change in total_cases
-# - reproduction_rate by last non-zero value
-# - extreme_poverty by median extreme_poverty value by continent
-# - rest of other predictors by last non-zero value
-
-data_tree3 = data_tree2 %>%
-  mutate(
-    new_deaths = ifelse(is.na(new_deaths), 0, new_deaths),
-    new_deaths_per_million = ifelse(is.na(new_deaths_per_million), 0, new_deaths_per_million)
-  )
-data_tree3 = data_tree3 %>%
-  group_by(location) %>%
-  fill(total_deaths, .direction = "downup") %>%
-  ungroup()
-data_tree3 = data_tree3 %>%
-  mutate(
-    total_deaths_per_million = ifelse(is.na(total_deaths_per_million), total_deaths / population * 1e6, total_deaths_per_million)
-  )
-data_tree3 = data_tree3 %>%
-  group_by(location) %>%
-  fill(total_cases, .direction = "downup") %>%
-  ungroup()
-data_tree3 = data_tree3 %>%
-  mutate(
-    total_cases_per_million = ifelse(is.na(total_cases_per_million), total_cases / population * 1e6, total_cases_per_million)
-  )
-data_tree3 = data_tree3 %>%
-  group_by(location) %>%
-  mutate(
-    new_cases = ifelse(is.na(new_cases), total_cases - lag(total_cases, default = 0), new_cases)
-  ) %>%
-  ungroup()
-data_tree3 = data_tree3 %>%
-  mutate(
-    new_cases_per_million = ifelse(is.na(new_cases_per_million), new_cases / population * 1e6, new_cases_per_million)
-  )
-data_tree3 = data_tree3 %>%
-  group_by(location) %>%
-  fill(reproduction_rate, .direction = "downup") %>%
-  ungroup()
-data_tree3 = data_tree3 %>% 
-  group_by(continent) %>% 
-  mutate(extreme_poverty = ifelse(is.na(extreme_poverty), median(extreme_poverty, na.rm = TRUE), extreme_poverty)) %>% 
-  ungroup()
-data_tree3 = data_tree3 %>% 
-  group_by(location) %>% 
-  mutate(stringency_index = ifelse(is.na(stringency_index), median(stringency_index, na.rm = TRUE), stringency_index)) %>% 
-  ungroup()
-
-
-data_tree3 = data_tree3 %>%
-  group_by(location) %>%
-  fill(total_tests, .direction = "downup") %>%
-  fill(positive_rate, .direction = "downup") %>%
-  fill(total_vaccinations, .direction = "downup") %>%
-  fill(people_vaccinated, .direction = "downup") %>%
-  mutate(new_tests = ifelse(is.na(new_tests), 0, new_tests)) %>%
-  ungroup()
-
-
-
-# View(data_tree3 %>% skim_without_charts())
-# View(cor(data_tree3 %>% select(-c(continent, location, date, G20, G24))))
-
-
-
+# Imputation for random missingness using clustering
+# refer to file clustering.R
 
 
 
---
+++ pacf_analysis.R
@@ -0,0 +1,109 @@
+# monthly average
+library(tseries)
+library(tidyverse)
+library(skimr)
+library(lubridate)
+
+data = read_csv('data/raw_data/owid-covid-data.csv')
+
+g20 = c('Argentina', 'Australia', 'Canada', 'China', 'France', 'Germany',
+        'India', 'Italy', 'Japan', 'South Korea', 'Mexico', 'Russia',
+        'Saudi Arabia', 'South Africa', 'Turkey', 'United Kingdom', 'United States')
+g24 = c('Argentina', 'China', 'Colombia', 'Ecuador', 'Ethiopia', 'India',
+        'Mexico', 'Morocco', 'Pakistan', 'Philippines', 'South Africa', 'Sri Lanka')
+data_cur = data %>%
+  filter(location %in% c(g20, g24)) %>%
+  mutate(G20 = location %in% g20, G24 = location %in% g24)
+
+
+xx = data_cur %>%
+  filter(location == "United States", date < as.Date('2023-05-01')) %>%
+  select(new_cases, date)
+xxx = xx %>%
+  group_by(date = floor_date(date, "month")) %>%
+  summarise(avg_new_cases = mean(new_cases, na.rm = TRUE))
+ggplot(xxx, aes(date, avg_new_cases)) +
+  geom_line() +
+  scale_x_date(breaks = "2 month") +
+  theme(axis.text.x = element_text(angle = 90))
+
+y = ts(data = xxx %>% select(avg_new_cases), start = 1, frequency = 12)
+acf(y, 40)
+acf(y, 40, type = "partial")
+adf.test(y)
+z = decompose(y, type = "multiplicative")
+z = decompose(y, type = "additive")
+plot(z)
+
+
+# new_cases monthly average Countries
+
+# zz = data_cur %>%
+#   filter(date < as.Date('2023-05-01')) %>%
+#   select(new_cases, date, location, G20, G24)
+# ggplot(zz %>% filter(G20), aes(date, new_cases)) +
+#   geom_line() +
+#   facet_wrap(~location, scales = "free_y")
+# ggplot(zz %>% filter(G24), aes(date, new_cases)) +
+#   geom_line() +
+#   facet_wrap(~location, scales = "free_y")
+
+thing = function(x) {
+  xx = x %>% select(new_cases, date)
+  xxx = xx %>%
+    group_by(date = floor_date(date, "month")) %>%
+    summarise(avg_new_cases = mean(new_cases, na.rm = TRUE))
+  ts(data = xxx %>% select(avg_new_cases), start = 1, frequency = 12)
+}
+
+z = data_cur %>%
+  filter(date < as.Date('2023-05-01')) %>%
+  select(new_cases, date, location)
+country = split(z, z$location)
+
+k = sapply(country, thing)
+
+country_acf = apply(k, 1, acf, lag.max = 40, plot = FALSE)
+names(country_acf) = names(country)
+
+country_pacf = apply(k, 1, acf, lag.max = 40, type = 'partial', plot = FALSE)
+names(country_pacf) = names(country)
+
+
+
+get_acf = function(x) {
+  as.vector(x[["acf"]])
+}
+
+m = as.data.frame(sapply(country_acf, get_acf)[,1:24]) %>%
+  mutate(lag = seq(0, 23, 1)) %>%
+  pivot_longer(everything() & !lag, names_to = "country", values_to = "acf") %>%
+  filter(lag > 0)
+n = as.data.frame(sapply(country_pacf, get_acf)[,1:24]) %>%
+  mutate(lag = seq(1, 23, 1)) %>%
+  pivot_longer(everything() & !lag, names_to = "country", values_to = "acf")
+
+
+ciz = (-qnorm((1-0.95)/2)/sqrt(1214-3))
+cir = (exp(2*ciz)-1)/(exp(2*ciz)+1)
+
+ggplot(m, aes(lag, 0)) +
+  geom_segment(aes(xend = lag, yend = acf), color = 'blue') +
+  geom_hline(yintercept = 0, color = 'black') +
+  geom_hline(yintercept = cir, color = 'red', linetype = 'dashed') +
+  geom_hline(yintercept = -1*cir, color = 'red', linetype = 'dashed') +
+  facet_wrap(~country, scales = "free_y") +
+  labs(title = "ACF of Average Monthly New Cases per Country", y = 'ACF')
+
+ggplot(n, aes(lag, 0)) +
+  geom_segment(aes(xend = lag, yend = acf), color = 'blue') +
+  geom_hline(yintercept = 0, color = 'black') +
+  geom_hline(yintercept = cir, color = 'red', linetype = 'dashed') +
+  geom_hline(yintercept = -1*cir, color = 'red', linetype = 'dashed') +
+  facet_wrap(~country, scales = "free_y") +
+  labs(title = "PACF of Average Monthly New Cases per Country", y = 'PACF')
+
+
+
+
+
