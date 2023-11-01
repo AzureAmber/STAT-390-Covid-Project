bce4556cdf80ab89684f013f5fcf7501fe663815
AzureAmber <TokyoGhoul4life1@gmail.com>
2023-11-01 01:05:25 -0500

    fixed training/test for linear models

--- Preprocessing Code/preprocess_lm.R
+++ Preprocessing Code/preprocess_lm.R
@@ -39,59 +39,13 @@ test_lm <- data_lm |> arrange(date) %>% filter(date >= as.Date("2023-01-01"))
 
 
 
-
-# Imputation for random missingness
-# Either impute using process below   OR    impute by clustering
-# - new_deaths impute with 0, total deaths by last non-zero value
-# - total_cases by last non-zero value, new_cases by change in total_cases
-# - reproduction_rate by last non-zero value
-# - extreme_poverty by median extreme_poverty value by continent
-# - rest of other predictors by last non-zero value
-
-#   ***** REPLACE data_lm here with the training set *****
-data_lm2 = train_lm  %>%
-  mutate(
-    new_deaths = ifelse(is.na(new_deaths), 0, new_deaths),
-    new_deaths_per_million = ifelse(is.na(new_deaths_per_million), 0, new_deaths_per_million)
-  )
-data_lm2 = data_lm2 %>%
-  group_by(location) %>%
-  fill(total_deaths, .direction = "downup") %>%
-  ungroup()
-data_lm2 = data_lm2 %>%
-  mutate(
-    total_deaths_per_million = ifelse(is.na(total_deaths_per_million), total_deaths / population * 1e6, total_deaths_per_million)
-  )
-data_lm2 = data_lm2 %>%
-  group_by(location) %>%
-  fill(total_cases, .direction = "downup") %>%
-  ungroup()
-data_lm2 = data_lm2 %>%
-  mutate(
-    total_cases_per_million = ifelse(is.na(total_cases_per_million), total_cases / population * 1e6, total_cases_per_million)
-  )
-data_lm2 = data_lm2 %>%
-  group_by(location) %>%
-  mutate(
-    new_cases = ifelse(is.na(new_cases), total_cases - lag(total_cases, default = 0), new_cases)
-  ) %>%
-  ungroup()
-data_lm2 = data_lm2 %>%
-  mutate(
-    new_cases_per_million = ifelse(is.na(new_cases_per_million), new_cases / population * 1e6, new_cases_per_million)
-  )
-data_lm2 = data_lm2 %>%
-  group_by(location) %>%
-  fill(reproduction_rate, .direction = "downup") %>%
-  ungroup()
-data_lm2 = data_lm2 %>% 
-  group_by(continent) %>% 
-  mutate(extreme_poverty = ifelse(is.na(extreme_poverty), median(extreme_poverty, na.rm = TRUE), extreme_poverty)) %>% 
-  ungroup()
-data_lm2 = data_lm2 %>% 
-  group_by(location) %>% 
-  mutate(stringency_index = ifelse(is.na(stringency_index), median(stringency_index, na.rm = TRUE), stringency_index)) %>% 
-  ungroup()
+# outlier check using cook's distance > 0.5
+model = lm(new_cases ~ location + new_deaths_per_million, data = data_lm)
+plot(model, which = 4)
+which(cooks.distance(model) > 0.5)
+# There is only one outlier with large leverage
+# new_deaths_per_million = Ecuador = 7324
+# There isn't much outliers so don't change anything
 
 
 # View(data_lm2 %>% skim_without_charts())
