2d74cef4b528cc216502cb78ede1d31de9768066
AzureAmber <TokyoGhoul4life1@gmail.com>
2023-11-02 14:08:02 -0500

    Fixed changes for finalized data for China

--- Preprocessing Code/preprocess_lm.R
+++ Preprocessing Code/preprocess_lm.R
@@ -10,10 +10,10 @@ library(factoextra)
 
 data = read_csv('data/raw_data/owid-covid-data.csv')
 
-g20 = c('Argentina', 'Australia', 'Canada', 'China', 'France', 'Germany',
+g20 = c('Argentina', 'Australia', 'Canada', 'France', 'Germany',
         'India', 'Italy', 'Japan', 'South Korea', 'Mexico', 'Russia',
         'Saudi Arabia', 'South Africa', 'Turkey', 'United Kingdom', 'United States')
-g24 = c('Argentina', 'China', 'Colombia', 'Ecuador', 'Ethiopia', 'India',
+g24 = c('Argentina', 'Colombia', 'Ecuador', 'Ethiopia', 'India',
         'Mexico', 'Morocco', 'Pakistan', 'Philippines', 'South Africa', 'Sri Lanka')
 data_cur = data %>%
   filter(location %in% c(g20, g24)) %>%
