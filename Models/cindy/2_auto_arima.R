298e6e268baa03c485cb799ebddaf436626c38d4
Cindy Ha <cindyha2025@u.northwestern.edu>
2023-11-06 03:05:22 -0600

    autoarima done; need to fix 1_arima still

--- Models/cindy/2_auto_arima.R
+++ Models/cindy/2_auto_arima.R
@@ -1,12 +1,9 @@
 library(tidyverse)
 library(tidymodels)
-library(doParallel)
-library(tictoc)
 library(forecast)
 library(tseries)
 library(modeltime)
-library(dials)
-library(timetk)
+library(doParallel)
 
 tidymodels_prefer()
 
@@ -27,55 +24,33 @@ data_folds <- rolling_origin(
   skip = 30*4,
   cumulative = FALSE
 )
-# 
-# # Testing another method
-# data_folds2 <- time_series_cv(
-#   data = train_lm,
-#   initial = 366,
-#   assess = 30 * 2,
-#   skip = 30 * 4,
-#   cumulative = FALSE
-# )
 
 # 3. Define model, recipe, and workflow
 autoarima_model <- arima_reg() %>% 
-  set_engine("auto_arima") %>% 
-  set_mode("regression")
+  set_engine("auto_arima") 
 
-autoarima_recipe <- recipe(new_cases ~ ., data = train_lm) %>%
-  step_rm(date) %>%
-  step_mutate(
-    G20 = as.integer(G20),
-    G24 = as.integer(G24)
-  ) %>%
-  step_dummy(all_nominal(), -all_outcomes())
+autoarima_recipe <- recipe(new_cases ~ date, data = train_lm) 
 
 autoarima_wflow <- workflow() %>%
   add_model(autoarima_model) %>%
   add_recipe(autoarima_recipe)
 
 # 4. Fit the model to the rolling origin resamples
-tic("auto arima fitting")
-
 autoarima_fits <- fit_resamples(
   autoarima_wflow,
   resamples = data_folds,
   control = control_resamples(save_pred = TRUE, save_workflow = TRUE, verbose = TRUE)
 )
 
-toc(log = TRUE)
-time_log <- tic.log(format = FALSE)
-autoarima_tictoc <- tibble(model = time_log[[1]]$msg, runtime = time_log[[1]]$toc - time_log[[1]]$tic)
+
+autoarima_fit
 
 stopCluster(cores.cluster)
 
-# 5. Save results
-save(autoarima_fits, autoarima_tictoc, file = "Models/cindy/results/autoarima_results1.rda")
-
-#########################################
-# 6. Looking at model results
-
+# 5. Results
 autoarima_fits |> 
   collect_metrics()
-  
 
+# .metric .estimator       mean     n    std_err .config             
+# 1 rmse    standard   43438.       205 3739.      Preprocessor1_Model1
+# 2 rsq     standard       0.0170   187    0.00314 Preprocessor1_Model1
