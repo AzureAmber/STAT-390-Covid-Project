0e46029373370a8a5e813a3058a7ddfc3d9ccd11
AzureAmber <TokyoGhoul4life1@gmail.com>
2023-11-07 03:43:17 -0600

    added auto arima

--- Models/willie/wx_autoarima.R
+++ Models/willie/wx_autoarima.R
@@ -3,10 +3,6 @@ library(tidymodels)
 library(modeltime)
 library(doParallel)
 
-# Source
-# https://www.r-bloggers.com/2020/06/introducing-modeltime-tidy-time-series-forecasting-using-tidymodels/
-
-
 # Setup parallel processing
 # detectCores(logical = FALSE)
 cores.cluster = makePSOCKcluster(20)
@@ -28,34 +24,34 @@ data_folds = rolling_origin(
 data_folds
 
 # 3. Define model, recipe, and workflow
-arima_model = arima_reg(
+autoarima_model = arima_reg(
   seasonal_period = 12,
   non_seasonal_ar = tune(), non_seasonal_differences = tune(), non_seasonal_ma = tune(),
   seasonal_ar = 1, seasonal_differences = tune(), seasonal_ma = 1) %>%
-  set_engine('arima')
+  set_engine('auto_arima')
 
-arima_recipe = recipe(new_cases ~ date, data = train_lm)
-# View(arima_recipe %>% prep() %>% bake(new_data = NULL))
+autoarima_recipe = recipe(new_cases ~ date, data = train_lm)
+# View(autoarima_recipe %>% prep() %>% bake(new_data = NULL))
 
-arima_wflow = workflow() %>%
-  add_model(arima_model) %>%
-  add_recipe(arima_recipe)
+autoarima_wflow = workflow() %>%
+  add_model(autoarima_model) %>%
+  add_recipe(autoarima_recipe)
 
 # 4. Setup tuning grid
-arima_params = arima_wflow %>%
+autoarima_params = autoarima_wflow %>%
   extract_parameter_set_dials() %>%
   update(
     non_seasonal_ar = non_seasonal_ar(c(0, 6)),
     non_seasonal_ma = non_seasonal_ma(c(0, 6)),
     seasonal_differences = seasonal_differences(c(0,2))
   )
-arima_grid = grid_regular(arima_params, levels = 3)
+autoarima_grid = grid_regular(autoarima_params, levels = 3)
 
 # 5. Model Tuning
-arima_tuned = tune_grid(
-  arima_wflow,
+autoarima_tuned = tune_grid(
+  autoarima_wflow,
   resamples = data_folds,
-  grid = arima_grid,
+  grid = autoarima_grid,
   control = control_grid(save_pred = TRUE,
                          save_workflow = FALSE,
                          parallel_over = "everything"),
@@ -64,28 +60,28 @@ arima_tuned = tune_grid(
 
 stopCluster(cores.cluster)
 
-arima_tuned %>% collect_metrics() %>%
+autoarima_tuned %>% collect_metrics() %>%
   group_by(.metric) %>%
   arrange(mean)
 
 # 6. Results
-autoplot(arima_tuned, metric = "rmse")
+autoplot(autoarima_tuned, metric = "rmse")
 
 # 7. Fit Best Model
 # Increase ar, ma
-arima_model = arima_reg(
+autoarima_model = arima_reg(
   seasonal_period = 12,
-  non_seasonal_ar = 6, non_seasonal_differences = 0, non_seasonal_ma = 3,
+  non_seasonal_ar = 5, non_seasonal_differences = 0, non_seasonal_ma = 5,
   seasonal_ar = 1, seasonal_differences = 0, seasonal_ma = 1) %>%
-  set_engine('arima')
-arima_recipe = recipe(new_cases ~ date, data = train_lm)
-arima_wflow = workflow() %>%
-  add_model(arima_model) %>%
-  add_recipe(arima_recipe)
+  set_engine('auto_arima')
+autoarima_recipe = recipe(new_cases ~ date, data = train_lm)
+autoarima_wflow = workflow() %>%
+  add_model(autoarima_model) %>%
+  add_recipe(autoarima_recipe)
 
-arima_fit = fit(arima_wflow, data = train_lm)
+autoarima_fit = fit(autoarima_wflow, data = train_lm)
 final_train = train_lm %>%
-  bind_cols(predict(arima_fit, new_data = train_lm)) %>%
+  bind_cols(predict(autoarima_fit, new_data = train_lm)) %>%
   rename(pred = .pred)
 
 ggplot(final_train %>% filter(location == "Argentina")) +
