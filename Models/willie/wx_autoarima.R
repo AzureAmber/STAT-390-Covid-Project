ea2b070aaefd839f4dee00c2a09c53008150abda
AzureAmber <TokyoGhoul4life1@gmail.com>
2023-11-08 05:44:11 -0600

    updated autoarima for us data

--- Models/willie/wx_autoarima.R
+++ Models/willie/wx_autoarima.R
@@ -4,67 +4,114 @@ library(modeltime)
 library(doParallel)
 library(RcppRoll)
 
-# Setup parallel processing
-# detectCores(logical = FALSE)
-cores.cluster = makePSOCKcluster(20)
-registerDoParallel(cores.cluster)
-
 
 # 1. Read in data
 train_lm = readRDS('data/finalized_data/final_train_lm.rds')
 test_lm = readRDS('data/finalized_data/final_test_lm.rds')
 
 # weekly rolling average
-train_lm_fix = train_lm %>%
-  filter(location == "United States") %>%
-  mutate(new_cases_scaled = roll_sum(new_cases, 7, align = "right", fill = 0) / 7) %>%
+train_lm = train_lm %>%
+  group_by(location) %>%
   arrange(date, .by_group = TRUE) %>%
-  ungroup() %>%
-  slice(which(row_number() %% 7 == 1))
-test_lm_fix = test_lm %>%
-  filter(location == "United States") %>%
-  mutate(new_cases_scaled = roll_sum(new_cases, 7, align = "right", fill = 0) / 7) %>%
+  mutate(value = roll_mean(new_cases, 7, align = "right", fill = NA)) %>%
+  mutate(value = ifelse(is.na(value), new_cases, value)) %>%
   arrange(date, .by_group = TRUE) %>%
+  slice(which(row_number() %% 7 == 1)) %>%
+  mutate(seasonality_group = row_number() %% 53) %>%
   ungroup() %>%
-  slice(which(row_number() %% 7 == 1))
+  mutate(seasonality_group = as.factor(seasonality_group))
+test_lm = test_lm %>%
+  group_by(location) %>%
+  arrange(date, .by_group = TRUE) %>%
+  mutate(value = roll_mean(new_cases, 7, align = "right", fill = NA)) %>%
+  mutate(value = ifelse(is.na(value), new_cases, value)) %>%
+  arrange(date, .by_group = TRUE) %>%
+  slice(which(row_number() %% 7 == 1)) %>%
+  mutate(seasonality_group = row_number() %% 53) %>%
+  ungroup() %>%
+  mutate(seasonality_group = as.factor(seasonality_group))
+complete_lm = train_lm %>% rbind(test_lm) %>%
+  group_by(location) %>%
+  arrange(date, .by_group = TRUE) %>%
+  ungroup()
 
-ggplot(train_lm_fix, aes(date, new_cases_scaled)) + geom_point()
+ggplot(train_lm %>% filter(location == "United States"), aes(date, value)) +
+  geom_point()
 
-# 2. Create validation sets for every year train + 2 month test with 4-month increments
+# 2. Find model trend by country
+train_lm_fix = NULL
+test_lm_fix = NULL
+country_names = unique(train_lm$location)
+for (i in country_names) {
+  data = train_lm %>% filter(location == i)
+  complete_data = complete_lm %>% filter(location == i)
+  # find linear model by country
+  lm_model = lm(value ~ 0 + time(date) + seasonality_group, data)
+  x = complete_data %>%
+    mutate(
+      trend = predict(lm_model, newdata = complete_data),
+      slope = as.numeric(coef(lm_model)["time(date)"]),
+      seasonality_add = trend - slope * time(date),
+      err = value - trend) %>%
+    mutate_if(is.numeric, round, 5)
+  train_lm_fix <<- rbind(train_lm_fix, x %>% filter(date < as.Date("2023-01-01")))
+  test_lm_fix <<- rbind(test_lm_fix, x %>% filter(date >= as.Date("2023-01-01")))
+}
+# plot of original data and trend
+ggplot(train_lm_fix %>% filter(location == "United States")) +
+  geom_line(aes(date, value), color = 'blue') +
+  geom_line(aes(date, trend), color = 'red')
+# plot of residual errors
+ggplot(x %>% filter(location == "United States"), aes(date, err)) + geom_line()
+
+
+
+
+
+# ARIMA Model tuning for errors
+# 3. Create validation sets for every year train + 2 month test with 4-month increments
+train_lm_fix = train_lm_fix %>% filter(location == "United States")
+
 data_folds = rolling_origin(
   train_lm_fix,
-  initial = 52,
+  initial = 53,
   assess = 4*2,
   skip = 4*4,
   cumulative = FALSE
 )
 data_folds
 
-# 3. Define model, recipe, and workflow
+# 4. Define model, recipe, and workflow
 autoarima_model = arima_reg(
-  seasonal_period = 52,
+  seasonal_period = 53,
   non_seasonal_ar = tune(), non_seasonal_differences = tune(), non_seasonal_ma = tune(),
   seasonal_ar = 1, seasonal_differences = tune(), seasonal_ma = 1) %>%
   set_engine('auto_arima')
 
-autoarima_recipe = recipe(new_cases_scaled ~ date, data = train_lm_fix)
+autoarima_recipe = recipe(err ~ date, data = train_lm_fix)
 # View(autoarima_recipe %>% prep() %>% bake(new_data = NULL))
 
 autoarima_wflow = workflow() %>%
   add_model(autoarima_model) %>%
   add_recipe(autoarima_recipe)
 
-# 4. Setup tuning grid
+# 5. Setup tuning grid
 autoarima_params = autoarima_wflow %>%
   extract_parameter_set_dials() %>%
   update(
+    non_seasonal_differences = non_seasonal_differences(c(0,2)),
     non_seasonal_ar = non_seasonal_ar(c(3, 5)),
     non_seasonal_ma = non_seasonal_ma(c(3, 5)),
-    seasonal_differences = seasonal_differences(c(0,1))
+    seasonal_differences = seasonal_differences(c(0,2))
   )
 autoarima_grid = grid_regular(autoarima_params, levels = 3)
 
-# 5. Model Tuning
+# 6. Model Tuning
+# Setup parallel processing
+# detectCores(logical = FALSE)
+cores.cluster = makePSOCKcluster(20)
+registerDoParallel(cores.cluster)
+
 autoarima_tuned = tune_grid(
   autoarima_wflow,
   resamples = data_folds,
@@ -78,39 +125,44 @@ autoarima_tuned = tune_grid(
 stopCluster(cores.cluster)
 
 autoarima_tuned %>% collect_metrics() %>%
+  relocate(mean) %>%
   group_by(.metric) %>%
   arrange(mean)
 
-# 6. Results
+# 7. Results
 autoplot(autoarima_tuned, metric = "rmse")
 
-# 7. Fit Best Model
-# ar = 3, d = 1, ma = 3, D = 0
+# 8. Fit Best Model
 autoarima_model = arima_reg(
-  seasonal_period = 52,
-  non_seasonal_ar = 3, non_seasonal_differences = 1, non_seasonal_ma = 3,
+  seasonal_period = 53,
+  non_seasonal_ar = 4, non_seasonal_differences = 0, non_seasonal_ma = 3,
   seasonal_ar = 1, seasonal_differences = 0, seasonal_ma = 1) %>%
   set_engine('auto_arima')
-autoarima_recipe = recipe(new_cases_scaled ~ date, data = train_lm_fix)
+autoarima_recipe = recipe(err ~ date, data = train_lm_fix)
 autoarima_wflow = workflow() %>%
   add_model(autoarima_model) %>%
   add_recipe(autoarima_recipe)
 
 autoarima_fit = fit(autoarima_wflow, data = train_lm_fix)
 final_train = train_lm_fix %>%
-  bind_cols(predict(autoarima_fit, new_data = train_lm_fix)) %>%
-  rename(pred = .pred)
+  bind_cols(pred_err = autoarima_fit$fit$fit$fit$data$.fitted) %>%
+  mutate(pred = trend + pred_err) %>%
+  mutate_if(is.numeric, round, 5)
 
+# error model
 ggplot(final_train) +
-  geom_line(aes(date, new_cases_scaled), color = 'red') +
-  geom_line(aes(date, pred), color = 'blue', linetype = "dashed") +
+  geom_line(aes(date, err), color = 'blue') +
+  geom_line(aes(date, pred_err), color = 'red', linetype = "dashed") +
+  scale_y_continuous(n.breaks = 15)
+# prediction model
+ggplot(final_train) +
+  geom_line(aes(date, value), color = 'blue') +
+  geom_line(aes(date, pred), color = 'red', linetype = "dashed") +
   scale_y_continuous(n.breaks = 15)
 
 library(ModelMetrics)
-results = final_train %>%
-  group_by(location) %>%
-  summarise(value = rmse(new_cases_scaled, pred)) %>%
-  arrange(location)
+rmse(final_train$err, final_train$pred_err)
+rmse(final_train$value, final_train$pred)
 
 
 
